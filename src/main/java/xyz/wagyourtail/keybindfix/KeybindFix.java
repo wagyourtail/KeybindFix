22a995836133c85918721adbb4914e093d3fb203
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-02-17 00:37:55 -0700

    version 1.0

--- src/main/java/xyz/wagyourtail/keybindfix/KeybindFix.java
+++ src/main/java/xyz/wagyourtail/keybindfix/KeybindFix.java
@@ -1,17 +1,30 @@
-package xyz.wagyourtail.sprintctrl;
+package xyz.wagyourtail.keybindfix;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.client.settings.KeyBinding;
-import org.lwjgl.input.Keyboard;
 
 @Mod(modid = KeybindFix.MODID, name = "Keybind Fix", version = "1.0.0", acceptedMinecraftVersions = "[1.7.10]")
 public class KeybindFix {
-    public static final String MODID = "sprintctrl";
+    public static final String MODID = "keybindfix";
 
-    public KeybindFix() {
+    private static final Multimap<Integer, KeyBinding> keyFixMap = ArrayListMultimap.create();
 
+    public static void putKey(Integer key, KeyBinding keyBind) {
+        keyFixMap.put(key, keyBind);
+    }
+
+    public static void clearMap() {
+        keyFixMap.clear();
+    }
+
+    public static void onKeyPressed(Integer key) {
+        keyFixMap.get(key).forEach(it -> it.pressTime++);
+    }
+
+    public static void setPressed(Integer key, boolean pressed) {
+        keyFixMap.get(key).forEach(it -> it.pressed = pressed);
     }
 
 }
--- src/main/java/xyz/wagyourtail/keybindfix/mixins/KeyBindingMixin.java
+++ src/main/java/xyz/wagyourtail/keybindfix/mixins/KeyBindingMixin.java
@@ -1,2 +1,44 @@
-package xyz.wagyourtail.keybindfix.mixins;public class KeyBindingMixin {
+package xyz.wagyourtail.keybindfix.mixins;
+
+import net.minecraft.client.settings.KeyBinding;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.wagyourtail.keybindfix.KeybindFix;
+
+import java.util.List;
+
+@Mixin(KeyBinding.class)
+public class KeyBindingMixin {
+
+    @Shadow @Final private static List keybindArray;
+
+    @Inject(method = "onTick", at = @At("HEAD"), cancellable = true)
+    private static void keybindfix_onKeyPressedFixed(int keycode, CallbackInfo ci) {
+        KeybindFix.onKeyPressed(keycode);
+        ci.cancel();
+    }
+
+    @Inject(method = "setKeyBindState", at = @At("HEAD"), cancellable = true)
+    private static void keybindfix_setPressedFixed(int keycode, boolean pressed, CallbackInfo ci) {
+        KeybindFix.setPressed(keycode, pressed);
+        ci.cancel();
+    }
+
+    @Inject(method = "resetKeyBindingArrayAndHash", at = @At("TAIL"))
+    private static void keybindfix_clearMap(CallbackInfo ci) {
+        KeybindFix.clearMap();
+        for (KeyBinding keyBinding : (List<KeyBinding>) keybindArray) {
+            KeybindFix.putKey(keyBinding.getKeyCode(), keyBinding);
+        }
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void keybindfix_putKey(String name, int keyCode, String category, CallbackInfo ci) {
+        KeybindFix.putKey(keyCode, (KeyBinding) (Object) this);
+    }
+
 }
