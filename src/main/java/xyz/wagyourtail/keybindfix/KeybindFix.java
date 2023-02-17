c501420a7c0cc70200e1001567764f8225871571
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-02-17 00:37:00 -0700

    Initial commit

---
+++ src/main/java/xyz/wagyourtail/keybindfix/KeybindFix.java
@@ -0,0 +1,17 @@
+package xyz.wagyourtail.sprintctrl;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.client.settings.KeyBinding;
+import org.lwjgl.input.Keyboard;
+
+@Mod(modid = KeybindFix.MODID, name = "Keybind Fix", version = "1.0.0", acceptedMinecraftVersions = "[1.7.10]")
+public class KeybindFix {
+    public static final String MODID = "sprintctrl";
+
+    public KeybindFix() {
+
+    }
+
+}
